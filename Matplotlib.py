eaaeca0b4e8151c993e9f896ae44d113afb80000
YYGod0120 <2569630663@qq.com>
2022-11-02 23:27:48 +0800

    得到数据，图还没画完

--- Matplotlib.py
+++ Matplotlib.py
@@ -1,22 +1,28 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+#创建一些数据
 score = np.random.randint(0,100,(10,6))
-print(score)
 score_df = pd.DataFrame(score) #数据结构
 col = ["语文","数学","英语","物理","化学","体育"] #行索引序列
 ide = ['同学'+str(i) for i in range(1,11)] #列索引
-
 data = pd.DataFrame(score,columns=col,index=ide)
-print(data)
+#创建一个画板进行作画
+plt.figure(figsize=(20,20),dpi=100)
+#获取你需要的数据
+colors = ['red','gray','pink','blue','green','black','orange','purple','#ccc',"#33cccc"]
 x = [i0 for i0 in range(1,11)]
 y = []
-plt.figure(figsize=(20,20),dpi=100)
-#创建画布等画图前提要素
 for a in range(0,6):
     for b in range(0,10):
         y.append(score[b,a])
+    plt.legend(f"同学")
+    plt.scatter(x,y,color=colors[b],marker='.')
+    y = []
+plt.show()
 
-    print(y)
+
+
+
 
 
